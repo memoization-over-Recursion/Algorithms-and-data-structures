feb98673f5e3a957ed30951d48fc84ac836c0ba6
memoization-over-Recursion <memoizationnotmemorisation@gmail.com>
2022-11-03 22:28:07 +0000

    dynamic question

---
+++ minNumOfJumps/minimumNumberOfJumpsNaive.java
@@ -0,0 +1,20 @@
+package minNumOfJumps;
+import java.util.*;
+// time complexity O( n^2 )
+// space complexity O( 1 )
+public class minimumNumberOfJumpsNaive {
+    public static int minNumberOfJumps(int[] array) {
+        int[] copy = new int[array.length];
+        Arrays.fill(copy, Integer.MAX_VALUE);
+        copy[0] = 0;
+        for (int i = 1; i < array.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (array[j] + j >= i) {
+                    copy[i] = Math.min(copy[i], copy[j] + 1);
+                }
+            }
+        }
+        return copy[copy.length - 1];
+
+    }
+}
